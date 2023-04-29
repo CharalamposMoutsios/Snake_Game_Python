d020f3e27db4b2b944113bbe163a20dba9b2093d
CharalamposMoutsios <Charalampos.Moutsios@yh.nackademin.se>
2023-04-30 01:01:10 +0200

    snake

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Snake_Game_Python
+
+working but still needs a lot of progress.
---
+++ snake.py
@@ -0,0 +1,81 @@
+import pygame
+import random
+
+class Snake:
+    def __init__(self):
+        self.size = 1
+        self.elements = [[100, 50]]
+        self.radius = 10
+        self.dx = 5
+        self.dy = 0
+        
+    def draw(self):
+        for element in self.elements:
+            pygame.draw.circle(screen, white, element, self.radius)
+            
+    def move(self):
+        for i in range(len(self.elements) - 1, 0, -1):
+            self.elements[i][0] = self.elements[i - 1][0]
+            self.elements[i][1] = self.elements[i - 1][1]
+            
+        self.elements[0][0] += self.dx
+        self.elements[0][1] += self.dy
+
+class Food:
+    def __init__(self):
+        self.position = [random.randrange(0, width - 10, 10), random.randrange(0, height - 10, 10)]
+        self.radius = 5
+        
+    def draw(self):
+        pygame.draw.circle(screen, red, self.position, self.radius)
+
+pygame.init()
+
+width = 500
+height = 500
+
+screen = pygame.display.set_mode((width, height))
+pygame.display.set_caption('Snake Game')
+
+clock = pygame.time.Clock()
+
+black = (0, 0, 0)
+white = (255, 255, 255)
+red = (255, 0, 0)
+
+snake = Snake()
+food = Food()
+
+while True:
+    screen.fill(black)
+    
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            quit()
+            
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                snake.dx = -5
+                snake.dy = 0
+            elif event.key == pygame.K_RIGHT:
+                snake.dx = 5
+                snake.dy = 0
+            elif event.key == pygame.K_UP:
+                snake.dx = 0
+                snake.dy = -5
+            elif event.key == pygame.K_DOWN:
+                snake.dx = 0
+                snake.dy = 5
+                
+    snake.move()
+    snake.draw()
+    food.draw()
+    
+    if abs(snake.elements[0][0] - food.position[0]) < 15 and abs(snake.elements[0][1] - food.position[1]) < 15:
+        snake.size += 1
+        snake.elements.append([0, 0])
+        food.position = [random.randrange(0, width - 10, 10), random.randrange(0, height - 10, 10)]
+        
+    pygame.display.update()
+    clock.tick(30)
